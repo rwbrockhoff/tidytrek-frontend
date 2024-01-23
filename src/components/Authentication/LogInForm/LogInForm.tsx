093dcec0b43e33984c60ed149c74baad1362c9a0
rwbrockhoff <brockhoffrw@gmail.com>
2024-01-23 15:23:43 -0700

    minor: change prettier config, testing syntax fit

--- src/components/Authentication/LogInForm/LogInForm.tsx
+++ src/components/Authentication/LogInForm/LogInForm.tsx
@@ -1,135 +1,119 @@
 import { Link } from 'react-router-dom';
-import {
-  Button,
-  Form,
-  Grid,
-  Header,
-  Icon,
-  Message,
-  Segment,
-} from 'semantic-ui-react';
+import { Button, Form, Grid, Header, Icon, Message, Segment } from 'semantic-ui-react';
 import './LogInForm.css';
 
 type FormProps = {
-  isRegisterForm: boolean;
-  isLoading: boolean;
-  formError: boolean;
-  formErrorMessage: string;
-  onFormChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  onSubmit: () => void;
+	isRegisterForm: boolean;
+	isLoading: boolean;
+	formError: boolean;
+	formErrorMessage: string;
+	onFormChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+	onSubmit: () => void;
 };
 
 const LogInForm = ({
-  isRegisterForm,
-  isLoading,
-  formError,
-  formErrorMessage,
-  onFormChange,
-  onSubmit,
+	isRegisterForm,
+	isLoading,
+	formError,
+	formErrorMessage,
+	onFormChange,
+	onSubmit,
 }: FormProps) => {
-  return (
-    <Grid.Column style={{ maxWidth: 450 }}>
-      <Header as="h1" textAlign="center">
-        tidytrek app
-      </Header>
-      <Form size="large">
-        <Segment stacked>
-          <Header as="h2" color="blue" textAlign="center">
-            {isRegisterForm
-              ? 'Register your account'
-              : 'Log-in to your account'}
-          </Header>
-          {isRegisterForm && (
-            <>
-              <Form.Input
-                fluid
-                icon="user"
-                iconPosition="left"
-                placeholder="Name"
-                name="name"
-                data-testid="name-input"
-                type="name"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-                  onFormChange(e)
-                }
-              />
-              <Form.Input
-                fluid
-                icon="user"
-                iconPosition="left"
-                placeholder="Username (optional)"
-                name="username"
-                data-testid="name-input"
-                type="name"
-                onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
-                  onFormChange(e)
-                }
-              />
-            </>
-          )}
-          <Form.Input
-            fluid
-            icon="at"
-            type="email"
-            iconPosition="left"
-            placeholder="E-mail address"
-            name="email"
-            data-testid="email-input"
-            onChange={onFormChange}
-          />
-          <Form.Input
-            fluid
-            icon="lock"
-            iconPosition="left"
-            placeholder="Password"
-            type="password"
-            name="password"
-            data-testid="password-input"
-            onChange={onFormChange}
-          />
-          {isRegisterForm && (
-            <Form.Input
-              fluid
-              icon="lock"
-              iconPosition="left"
-              placeholder="Verify password"
-              type="password"
-              name="confirmPassword"
-              data-testid="verify-password-input"
-              onChange={onFormChange}
-            />
-          )}
+	return (
+		<Grid.Column style={{ maxWidth: 450 }}>
+			<Header as="h1" textAlign="center">
+				tidytrek app
+			</Header>
+			<Form size="large">
+				<Segment stacked>
+					<Header as="h2" color="blue" textAlign="center">
+						{isRegisterForm ? 'Register your account' : 'Log-in to your account'}
+					</Header>
+					{isRegisterForm && (
+						<>
+							<Form.Input
+								fluid
+								icon="user"
+								iconPosition="left"
+								placeholder="Name"
+								name="name"
+								data-testid="name-input"
+								type="name"
+								onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+									onFormChange(e)
+								}
+							/>
+							<Form.Input
+								fluid
+								icon="user"
+								iconPosition="left"
+								placeholder="Username (optional)"
+								name="username"
+								data-testid="username-input"
+								type="name"
+								onChange={(e: React.ChangeEvent<HTMLInputElement>): void =>
+									onFormChange(e)
+								}
+							/>
+						</>
+					)}
+					<Form.Input
+						fluid
+						icon="at"
+						type="email"
+						iconPosition="left"
+						placeholder="E-mail address"
+						name="email"
+						data-testid="email-input"
+						onChange={onFormChange}
+					/>
+					<Form.Input
+						fluid
+						icon="lock"
+						iconPosition="left"
+						placeholder="Password"
+						type="password"
+						name="password"
+						data-testid="password-input"
+						onChange={onFormChange}
+					/>
+					{isRegisterForm && (
+						<Form.Input
+							fluid
+							icon="lock"
+							iconPosition="left"
+							placeholder="Verify password"
+							type="password"
+							name="confirmPassword"
+							data-testid="verify-password-input"
+							onChange={onFormChange}
+						/>
+					)}
 
-          <Button
-            color="blue"
-            fluid
-            size="large"
-            disabled={isLoading}
-            onClick={onSubmit}
-          >
-            {isRegisterForm ? 'Register' : 'Login'}
-          </Button>
+					<Button color="blue" fluid size="large" disabled={isLoading} onClick={onSubmit}>
+						{isRegisterForm ? 'Register' : 'Login'}
+					</Button>
 
-          {formError && (
-            <Message color="red" data-testid="error-message">
-              <Icon name="hand point right outline" />
-              {formErrorMessage || 'Oops! There was an error.'}
-            </Message>
-          )}
-          {isRegisterForm ? (
-            <p style={{ marginTop: '25px' }}>
-              Already have an account? <Link to={'/'}>Log In</Link>
-            </p>
-          ) : (
-            <p style={{ marginTop: '25px' }}>
-              <Link to={'/register'}>Sign Up</Link> |{' '}
-              <Link to={'/reset-password'}>Forgot Your Password</Link>
-            </p>
-          )}
-        </Segment>
-      </Form>
-    </Grid.Column>
-  );
+					{formError && (
+						<Message color="red" data-testid="error-message">
+							<Icon name="hand point right outline" />
+							{formErrorMessage || 'Oops! There was an error.'}
+						</Message>
+					)}
+					{isRegisterForm ? (
+						<p style={{ marginTop: '25px' }}>
+							Already have an account? <Link to={'/'}>Log In</Link>
+						</p>
+					) : (
+						<p style={{ marginTop: '25px' }}>
+							<Link to={'/register'}>Sign Up</Link> |{' '}
+							<Link to={'/reset-password'}>Forgot Your Password</Link>
+						</p>
+					)}
+				</Segment>
+			</Form>
+		</Grid.Column>
+	);
 };
 
 export default LogInForm;
