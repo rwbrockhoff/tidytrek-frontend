4cccb3c929b387b379ec6d0d70437e56e3917331
rwbrockhoff <brockhoffrw@gmail.com>
2024-01-27 10:32:20 -0700

    fix: remove ui flash on delete gear closet item

--- src/views/GearCloset/GearCloset.tsx
+++ src/views/GearCloset/GearCloset.tsx
@@ -1,25 +1,22 @@
-import { Header, Loader } from 'semantic-ui-react';
+import { Header } from 'semantic-ui-react';
 import './GearCloset.css';
 import GearClosetList from '../../components/GearCloset/GearClosetList/GearClosetList';
 import { useGetGearClosetQuery } from '../../redux/closet/closetApiSlice';
 
 const GearCloset = () => {
-	const { data, isLoading, isFetching } = useGetGearClosetQuery();
+	const { data } = useGetGearClosetQuery();
 	const { gearClosetList, availablePacks } = data || {
 		gearClosetList: [],
 		availablePacks: [],
 	};
-	const loading = isLoading || isFetching;
+
 	return (
 		<div>
 			<Header as="h1" textAlign="center" className="gear-closet-header">
 				Gear Closet
 			</Header>
-			{!loading && (
-				<GearClosetList gearClosetList={gearClosetList} availablePacks={availablePacks} />
-			)}
 
-			<Loader active={loading} inline="centered" />
+			<GearClosetList gearClosetList={gearClosetList} availablePacks={availablePacks} />
 		</div>
 	);
 };
