cdb0705b61b512b2070276fd0a04a3c31fa84a0d
rwbrockhoff <brockhoffrw@gmail.com>
2024-02-07 18:29:40 -0700

    style: GuestPackLayout -> center Outlet content

--- src/views/Layout/GuestPackLayout.tsx
+++ src/views/Layout/GuestPackLayout.tsx
@@ -3,7 +3,7 @@ import { Outlet } from 'react-router-dom';
 const GuestPackLayout = () => {
 	return (
 		<div id="app-view-container">
-			<div id="view-component-container">
+			<div id="guest-view-container">
 				<Outlet />
 			</div>
 		</div>
