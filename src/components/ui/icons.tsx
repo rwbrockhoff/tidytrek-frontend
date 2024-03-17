2892ac917fd5360cb6afe996b53274720036c67c
rwbrockhoff <brockhoffrw@gmail.com>
2024-03-17 16:50:19 -0600

    style: improve callout appearance for send email confirmation

--- src/components/ui/icons.tsx
+++ src/components/ui/icons.tsx
@@ -4,5 +4,7 @@ export {
 	FaTrash as TrashIcon,
 	FaCaretDown as CaretDownIcon,
 	FaShare as ShareIcon,
+	FaCheck as CheckIcon,
 } from 'react-icons/fa';
 export { TfiMenu as SidebarIcon } from 'react-icons/tfi';
+export { BsFillSendFill as SendIcon } from 'react-icons/bs';
--- src/features/account/components/account-form/confirmation-form.tsx
+++ src/features/account/components/account-form/confirmation-form.tsx
@@ -1,41 +1,47 @@
 import styled from 'styled-components';
-import { Icon, Message, MessageHeader } from 'semantic-ui-react';
-import { Button } from '@/components/ui/SemanticUI';
-import { SubText } from '@/components/ui/TidyUI';
+import { SendIcon, CheckIcon } from '@/components/ui';
+import { Callout, Button, Heading, Text } from '@radix-ui/themes';
 
 type ConfirmationFormProps = { sendConfirmation: () => void; confirmationSent: boolean };
 
-export const ConfirmationForm = ({
-	sendConfirmation,
-	confirmationSent,
-}: ConfirmationFormProps) => {
+export const ConfirmationForm = (props: ConfirmationFormProps) => {
+	const { sendConfirmation, confirmationSent } = props;
 	return (
 		<ConfirmationContainer>
-			{!confirmationSent ? (
-				<Message style={{ width: messageWidth }}>
-					<MessageHeader>Verify Email </MessageHeader>
-					<SubText>Let's send a code to your email to get started.</SubText>
-					<Button basic onClick={sendConfirmation}>
-						<Icon name="send" />
-						Send Code
-					</Button>
-				</Message>
+			{confirmationSent ? (
+				<Callout.Root color="grass" variant="surface">
+					<StyledHeading size="4">
+						<CheckIcon />
+						Email Sent!
+					</StyledHeading>
+					<Text mb="2">Check your email for your code to use below.</Text>
+				</Callout.Root>
 			) : (
-				<Message style={{ width: messageWidth }} success>
-					<MessageHeader>
-						<Icon name="check" />
-						Code Sent{' '}
-					</MessageHeader>
-					<SubText>Check your email for your code to use below.</SubText>
-				</Message>
+				<Callout.Root color="gray" variant="surface">
+					<Heading size="4">Verify Email</Heading>
+					<Text mb="2">Let's send a code to your email to get started.</Text>
+					<Button color="gray" variant="outline" onClick={sendConfirmation}>
+						<SendIcon />
+						Send Email
+					</Button>
+				</Callout.Root>
 			)}
 		</ConfirmationContainer>
 	);
 };
 
 const ConfirmationContainer = styled.div`
+	width: 40%;
 	margin: 2em 0em 2em 0em;
+	button {
+		cursor: pointer;
+	}
 `;
 
-// defaults
-const messageWidth = '50%';
+const StyledHeading = styled(Heading)`
+	display: flex;
+	align-items: center;
+	svg {
+		margin-right: 5px;
+	}
+`;
