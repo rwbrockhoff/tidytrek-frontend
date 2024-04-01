c8c880e354a9244d7e0616b7aa16a8d8e8b8b837
rwbrockhoff <brockhoffrw@gmail.com>
2024-04-01 15:29:48 -0600

    fix: clean up file upload, minor style fixes

--- src/components/upload-file.tsx
+++ src/components/upload-file.tsx
@@ -1,6 +1,6 @@
 import styled from 'styled-components';
 import { MdCloudUpload as UploadIcon } from 'react-icons/md';
-import { ChangeEvent, FormEvent, useRef, useState, useEffect } from 'react';
+import { ChangeEvent, useRef } from 'react';
 
 type UploadFileProps = {
 	fileId: string;
@@ -11,45 +11,25 @@ type UploadFileProps = {
 };
 
 export const UploadFile = (props: UploadFileProps) => {
-	const {
-		fileId,
-		fileType = 'image/jpg, image/png',
-		fileName,
-		isPending,
-		onUpload,
-	} = props;
+	const { fileId, fileType = 'image/*', fileName, isPending, onUpload } = props;
 
 	const formRef = useRef<HTMLFormElement | null>(null);
-	const [file, setFile] = useState<globalThis.File | null>();
-
-	useEffect(() => {
-		if (file && formRef?.current) {
-			formRef.current.requestSubmit();
-		}
-	}, [file]);
 
 	const handleFile = (e: ChangeEvent<HTMLInputElement>) => {
-		if (e.target.files) {
-			const file = e.target.files[0];
-			setFile(file);
-		}
-	};
-
-	const handleSubmitForm = async (e: FormEvent) => {
 		e.preventDefault();
-
-		if (file) {
-			const formData = new FormData();
+		e.stopPropagation();
+		if (e.target.files && e.target.files.length > 0 && formRef.current) {
+			const file = e.target.files[0];
+			const formData = new FormData(formRef.current);
 			formData.append(fileName, file);
 			onUpload && onUpload(formData);
 			formRef?.current && formRef.current.reset();
-			setFile(null);
 		}
 	};
 
 	return (
 		<div>
-			<form encType="multipart/form-data" ref={formRef} onSubmit={handleSubmitForm}>
+			<form encType="multipart/form-data" ref={formRef}>
 				<UploadLabel htmlFor={fileId}>
 					<UploadIcon />
 				</UploadLabel>
