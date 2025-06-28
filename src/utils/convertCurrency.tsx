b9a0e63a81b867f3a1edf6bfbe1ddd69db0ab8c0
rwbrockhoff <brockhoffrw@gmail.com>
2025-06-28 14:04:18 -0600

    performance: improve convertCurrency formatter, useMemo for pack chart data

--- src/utils/convertCurrency.tsx
+++ src/utils/convertCurrency.tsx
@@ -1,9 +1,20 @@
+// Cache currency formatter in object to avoid recreating them
+const formatters: Record<string, Intl.NumberFormat> = {};
+
+const getCurrencyFormatter = (currency: string): Intl.NumberFormat => {
+	if (!formatters[currency]) {
+		formatters[currency] = new Intl.NumberFormat('en-US', {
+			style: 'currency',
+			currency,
+		});
+	}
+	return formatters[currency];
+};
+
 export const convertCurrency = (price: number, currency: string = 'USD') => {
-	const currencyConverter = Intl.NumberFormat('en-US', {
-		style: 'currency',
-		currency,
-	});
 	const isInvalid = isNaN(price);
 	if (isInvalid) return price;
-	else return currencyConverter.format(price);
+
+	const formatter = getCurrencyFormatter(currency);
+	return formatter.format(price);
 };
