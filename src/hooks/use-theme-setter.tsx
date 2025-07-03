d2df88a85aaf357a9beef8cedd9574ba75665010
rwbrockhoff <brockhoffrw@gmail.com>
2025-07-03 09:24:23 -0600

    feat: include paletteThemeName in useThemeSetter hook

--- src/hooks/use-theme-setter.tsx
+++ src/hooks/use-theme-setter.tsx
@@ -1,29 +1,42 @@
 import { useEffect, useState } from 'react';
 
-export function useThemeSetter(darkMode?: boolean, themeName?: string) {
+export function useThemeSetter(darkMode?: boolean, paletteThemeName?: string) {
 	const [currentTheme, setCurrentTheme] = useState<'light' | 'dark'>('light');
 
 	const getPreferredTheme = (): 'light' | 'dark' => {
 		const savedTheme = localStorage.getItem('theme');
+		// Return saved theme
 		if (savedTheme && (savedTheme === 'light' || savedTheme === 'dark')) {
 			return savedTheme as 'light' | 'dark';
 		}
+		// Or else match users browser preference
 		return window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+	};
+
+	const getPreferredPalette = (): string => {
+		const savedPalette = localStorage.getItem('palette');
+		// Return saved palette
+		if (savedPalette) return savedPalette;
+
+		// Return default option as fallback
+		return 'earth-tones';
 	};
 
 	useEffect(() => {
 		// If user has database settings, use those; otherwise fall back to system preference
-
-		let themeToUse: 'light' | 'dark';
-		const paletteToUse = themeName || 'earth-tones';
+		let themeToUse: 'light' | 'dark'; // ts
+		let paletteToUse = 'earth-tones';
 
 		if (darkMode !== undefined) themeToUse = darkMode ? 'dark' : 'light';
 		else themeToUse = getPreferredTheme();
 
+		if (paletteThemeName) paletteToUse = paletteThemeName;
+		else paletteToUse = getPreferredPalette();
+
 		document.body.setAttribute('data-theme', themeToUse);
 		document.body.setAttribute('data-theme-palette', paletteToUse);
 		setCurrentTheme(themeToUse);
-	}, [darkMode, themeName]);
+	}, [darkMode, paletteThemeName]);
 
 	return currentTheme;
 }
