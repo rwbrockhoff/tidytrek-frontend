aebf7276e45655e29bf3c31670972377f5faa576
rwbrockhoff <brockhoffrw@gmail.com>
2025-07-24 15:35:25 -0600

    style(offline banner): improve mobile styles

--- src/components/ui/offline-banner.tsx
+++ src/components/ui/offline-banner.tsx
@@ -12,7 +12,7 @@ export const OfflineBanner = () => {
 		<div
 			className={cn(
 				styles.offlineBanner,
-				'fixed top-0 left-0 right-0 z-100 flex items-center justify-center',
+				'fixed left-0 right-0 z-50 flex items-center justify-center',
 			)}>
 			<WifiOffIcon />
 			<span>You're offline. Some features may not work.</span>
